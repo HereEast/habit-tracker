2cfacaf5b5b62c1537a5540016396f9bbb873e78
Nastia Piven <nastiapiven@gmail.com>
2024-08-27 23:01:42 +0200

    feat: test request

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
   console.log("Hello");
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await fetch("http://localhost:5050/api"); 
+        if (!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        setTodos(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchTodos(); 
+  }, []);
 
   return (
     <main className={"flex min-h-screen flex-col items-center justify-between p-24"}>
       <p className="bg-red-300 px-6 py-2">Test again 2222</p>
+      <p>{todos[0]?.todo}</p>
     </main>
   );
 }
