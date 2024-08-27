36930ab2dc09d088c0f61b39c90cb8974fdd2832
Nastia Piven <nastiapiven@gmail.com>
2024-08-27 22:26:37 +0200

    feat: test

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 
   return (
     <main className={"flex min-h-screen flex-col items-center justify-between p-24"}>
-      <p className="bg-red-300 px-6 py-2">Test again</p>
+      <p className="bg-red-300 px-6 py-2">Test again 2222</p>
     </main>
   );
 }
