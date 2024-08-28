a7feb819ff56d306ce651533a3dac0e13127230b
Nastia Piven <nastiapiven@gmail.com>
2024-08-28 22:49:58 +0200

    feat: update express to connect db with mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,31 @@
-import express, { type Express } from "express";
+import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 import * as connect from "./mongodb/connect.js";
-import { todosRouter } from "./api/todos.js";
+import { taskRouter } from "./api/tasks.js";
 
 const PORT = process.env.PORT || 5050;
 
-const app: Express = express();
+const app = express();
 
 app.use(cors()); // connect fe and be since they are using different PORTs
 app.use(express.json()); // parse data in json format
-app.use(todosRouter);
+app.use(taskRouter);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is listening on port ${PORT}.`);
+start();
 
-  connect.connectToServer();
-});
+async function start() {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING || "");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is listening on port ${PORT}.`);
+
+      connect.connectToServer();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
