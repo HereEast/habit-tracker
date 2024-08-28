fdc281b087ee9ec5990b45eb752a2d124276678b
Nastia Piven <nastiapiven@gmail.com>
2024-08-28 22:50:33 +0200

    feat: add Task schema

---
+++ server/src/mongodb/models/User.ts
@@ -0,0 +1,14 @@
+import mongoose, { InferSchemaType } from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+  },
+  { timestamps: true },
+);
+
+type UserType = InferSchemaType<typeof UserSchema>;
+
+export default mongoose.model<UserType>("User", UserSchema);
