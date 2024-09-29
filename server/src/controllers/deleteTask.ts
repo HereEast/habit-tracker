dbb9ba16bc872f957bc7b7e8cebd8ce5655a410c
Nastia Piven <nastiapiven@gmail.com>
2024-09-29 17:47:02 +0200

    feat: update deleteTask endpoint

--- server/src/controllers/deleteTask.ts
+++ server/src/controllers/deleteTask.ts
@@ -2,31 +2,52 @@ import mongoose, { Error } from "mongoose";
 import { Request, Response } from "express";
 
 import { Task } from "../models/Task.js";
-import { Entry } from "../models/Entry.js";
 import { User } from "../models/User.js";
 
 // Delete task by ID
 export async function deleteTask(req: Request, res: Response) {
   const { userId, taskId } = req.params;
 
-  try {
-    await Task.deleteOne({ _id: taskId }).exec();
-    await Entry.deleteMany({ taskId }).exec();
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth();
 
+  try {
     const user = await User.findById(userId).exec();
 
     if (!user) {
-      return res.status(404).json({ message: "User not found while deleting a task." });
+      return res.status(404).json({ message: "User was not found while deleting a task." });
     }
 
-    const mongoTaskId = new mongoose.Types.ObjectId(taskId);
-    const updatedTasks = user.tasks.filter((id) => !id.equals(mongoTaskId));
+    const taskObjectId = new mongoose.Types.ObjectId(taskId);
 
-    user.tasks = updatedTasks;
+    // Delete from the current month
+    const timelineYear = user.timeline.find((entry) => entry.year === currentYear);
+    const timelineMonth = timelineYear?.months.find((mon) => mon.month === currentMonth + 1);
+
+    if (!timelineMonth) {
+      return;
+    }
+
+    const updatedTasks = timelineMonth.tasks.filter((id) => !id.equals(taskObjectId));
+    timelineMonth.tasks = updatedTasks;
+
     await user.save();
 
+    // Set stopped flag and stoppedAt date
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task was not found while deleting a task." });
+    }
+
+    task.stopped = true;
+    task.stoppedAt = today;
+
+    await task.save();
+
     return res.status(201).json({
-      message: `Task with ID ${taskId} is deleted.`,
+      message: `Task with ID ${taskId} was deleted from the current month.`,
     });
   } catch (err) {
     if (err instanceof Error) {
