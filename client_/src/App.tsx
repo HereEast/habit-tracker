4be9282905a5571f3d6bb267cb4fdb327bcb72c0
Nastia Piven <nastiapiven@gmail.com>
2025-02-28 17:40:46 +0100

    feat: install tailwind

--- client_/src/App.tsx
+++ client_/src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="font-sans">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
