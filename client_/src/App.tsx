8ce66ea5433a1894ee3c4f25061678ac693e7f76
Nastia Piven <nastiapiven@gmail.com>
2025-02-28 19:15:56 +0100

    feat: setup tailwind plugins

--- client_/src/App.tsx
+++ client_/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export function App() {
   const [count, setCount] = useState(0);
@@ -7,19 +7,16 @@ export function App() {
 
   return (
     <>
-      <h1 className="font-sans">Vite + React</h1>
-      <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="m-10 p-4 font-sans">Vite + React</h1>
+      <div className="px-4 py-2 text-white sm:px-8 sm:py-3">
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-
-      </p>
+      <p className="">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
