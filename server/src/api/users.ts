cb6477a6f324807934e4586b130f1b1ead9c871a
Nastia Piven <nastiapiven@gmail.com>
2025-03-02 20:07:24 +0100

    feat: wip

--- server/src/api/users.ts
+++ server/src/api/users.ts
@@ -3,15 +3,11 @@ import express from "express";
 import { createUser } from "../controllers/createUser.js";
 import { getUser } from "../controllers/getUsers.js";
 import { updateUser } from "../controllers/updateUser.js";
-// import { getUserYear } from "../controllers/getUserYear.js";
 import { login, verifyToken } from "../controllers/login.js";
 
 const router = express.Router();
 
 // Get
-// router.route("/timeline/:year").get(getUserYear);
-
-// router.route("/timeline/:year").get(verifyToken, getUserYear);
 router.route("/:slug").get(getUser);
 
 // Create
