5e6e5ffab951deca67b609d16c10699da2b74ee8
Nastia Piven <nastiapiven@gmail.com>
2025-03-06 21:50:35 +0100

    refactor: refactor

--- client/src/components/month-card/MonthCard.tsx
+++ client/src/components/month-card/MonthCard.tsx
@@ -1,16 +1,52 @@
+import { Notice } from "../Notice";
 import { MonthCardHeader } from "./MonthCardHeader";
+import { MonthDays } from "./MonthDays";
+import { TaskEntries } from "./TaskEntries";
+import { CreateTaskForm } from "../CreateTaskForm";
 
-import { IMonthData } from "~/utils/types";
+import { MonthTimelineData } from "~/utils/types";
+import { isCurrentMonth } from "~/utils/handlers";
+import { IEntry } from "~/server/models/Entry";
 
 interface MonthCardProps {
   year: number;
-  monthData: IMonthData;
+  monthData: MonthTimelineData;
 }
 
 export function MonthCard({ year, monthData }: MonthCardProps) {
+  const { month, tasks: monthTasks } = monthData;
+
   return (
-    <div className="w-fit min-w-[680px] space-y-6 rounded-xl bg-stone-100/75 p-6">
-      <MonthCardHeader year={year} month={monthData.month} />
+    <div className="w-fit min-w-[680px] rounded-xl bg-stone-100/75 p-6">
+      <MonthCardHeader year={year} month={month} />
+      <MonthDays year={year} month={month} />
+
+      <div>
+        {monthTasks?.length === 0 && (
+          <Notice text="You haven't created any tasks yet." />
+        )}
+      </div>
+
+      {monthTasks && (
+        <ul className="mb-6 space-y-0.5">
+          {monthTasks.map(({ task }, index) => {
+            const monthEntries = task.entries as IEntry[];
+
+            return (
+              <li className="flex w-full items-center gap-6" key={index}>
+                <div className="w-32">
+                  <h3>{task.title}</h3>
+                </div>
+
+                <TaskEntries entries={monthEntries} year={year} month={month} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {/* Form */}
+      {isCurrentMonth(year, month) && <CreateTaskForm />}
     </div>
   );
 }
--- client/src/pages/Timeline.tsx
+++ client/src/pages/Timeline.tsx
@@ -1,15 +1,10 @@
 import { useParams } from "react-router-dom";
 
-import { CreateTaskForm } from "~/components/CreateTaskForm";
-import { MonthCardHeader } from "~/components/month-card/MonthCardHeader";
-import { MonthDays } from "~/components/month-card/MonthDays";
-import { TaskEntries } from "~/components/month-card/TaskEntries";
-import { Notice } from "~/components/Notice";
 import { RatingButtons } from "~/components/RatingButtons";
+import { MonthCard } from "~/components/month-card/MonthCard";
 
 import { useUser, useYearData } from "~/hooks/queries";
-import { getToday, isCurrentMonth } from "~/utils/handlers";
-import { IEntry } from "~/server/models/Entry";
+import { getToday } from "~/utils/handlers";
 
 export function Timeline() {
   const { slug } = useParams();
@@ -27,106 +22,9 @@ export function Timeline() {
 
       <div>
         {timeline?.map((data, index) => (
-          <div
-            className="w-fit min-w-[680px] rounded-xl bg-stone-100/75 p-6"
-            key={index}
-          >
-            <MonthCardHeader year={currentYear} month={data.month} />
-            <MonthDays year={currentYear} month={data.month} />
-
-            <div>
-              {data.tasks?.length === 0 && (
-                <Notice text="You haven't created any tasks yet." />
-              )}
-            </div>
-
-            {data.tasks && (
-              <ul className="mb-6 space-y-0.5">
-                {data.tasks.map(({ task }, index) => {
-                  const monthEntries = task.entries as IEntry[];
-
-                  return (
-                    <li className="flex w-full items-center gap-6" key={index}>
-                      <div className="w-32">
-                        <h3>{task.title}</h3>
-                      </div>
-
-                      <TaskEntries
-                        entries={monthEntries}
-                        year={currentYear}
-                        month={data.month}
-                      />
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
-
-            {/* Form */}
-            {isCurrentMonth(currentYear, data.month) && <CreateTaskForm />}
-          </div>
+          <MonthCard year={currentYear} monthData={data} key={index} />
         ))}
       </div>
     </div>
   );
 }
-
-// export function Timeline() {
-//   const { slug } = useParams();
-
-//   const { currentMonth, currentYear } = getToday();
-
-//   const { data: user, isError } = useUser(slug!);
-//   const { data: tasks } = useUserTasks(String(user?._id || ""));
-
-//   // Year data
-//   const { data: timeline } = useYearData(String(user?._id || ""), currentYear);
-
-//   console.log("TIMELINE", timeline);
-
-//   return (
-//     <div className="flex flex-col items-center gap-6">
-//       <RatingButtons />
-
-//       <div>
-//         <div className="w-fit min-w-[680px] rounded-xl bg-stone-100/75 p-6">
-//           <MonthCardHeader year={currentYear} month={currentMonth} />
-//           <MonthDays year={currentYear} month={currentMonth} />
-
-//           <div>
-//             {tasks?.length === 0 && (
-//               <Notice text="You haven't created any tasks yet." />
-//             )}
-//           </div>
-
-//           {tasks && tasks?.length > 0 && (
-//             <ul className="mb-6 space-y-0.5">
-//               {tasks.map((task) => (
-//                 <li
-//                   className="flex w-full items-center gap-6"
-//                   key={String(task._id)}
-//                 >
-//                   <div className="w-32">
-//                     <h3>{task.title}</h3>
-//                   </div>
-
-//                   <TaskEntries
-//                     userId={String(user?._id || "")}
-//                     taskId={String(task._id)}
-//                     year={currentYear}
-//                     month={currentMonth}
-//                   />
-//                 </li>
-//               ))}
-//             </ul>
-//           )}
-
-//           {/* Form */}
-//           {/* {isCurrentMonth(currentYear, data.month) && <div>Form</div>} */}
-
-//           <CreateTaskForm />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
