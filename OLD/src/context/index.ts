446c1965da339b767cb1b3cda0e2890ae62f80cc
Nastia Piven <nastiapiven@gmail.com>
2025-03-10 11:48:29 +0100

    feat: delete old

--- OLD/src/context/index.ts
+++
@@ -1,3 +0,0 @@
-export { AppContext, AppContextProvider } from "./appContext";
-export { MonthContext, MonthContextProvider } from "./monthContext";
-export { AuthContext, AuthContextProvider } from "./authContext";
