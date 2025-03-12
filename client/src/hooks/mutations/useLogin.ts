9f2f456d66c578adfc1ad50c7c75b438786071b5
Nastia Piven <nastiapiven@gmail.com>
2025-03-12 19:32:56 +0100

    fix: fix redirect on login

--- client/src/hooks/mutations/useLogin.ts
+++ client/src/hooks/mutations/useLogin.ts
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { JwtPayload } from "jsonwebtoken";
 import { jwtDecode } from "jwt-decode";
@@ -6,21 +5,16 @@ import { jwtDecode } from "jwt-decode";
 import { login } from "~/api/login";
 
 export function useLogin() {
-  const navigate = useNavigate();
-
   const { mutate, error } = useMutation({
     mutationKey: ["user"],
     mutationFn: login,
     onSuccess: (data) => {
       localStorage.setItem("token", data.token);
 
-      console.log("Success", data);
-
       if (data) {
         const decodedUser = jwtDecode(data.token) as JwtPayload;
-
-        console.log("Decoded", decodedUser.username);
-        navigate(`/${decodedUser.username}`);
+        
+        window.location.replace(`/${decodedUser.username}`);
       }
     },
   });
