4508209c09c3bedc6f6cd5a977269843d1c07fad
Nastia Piven <nastiapiven@gmail.com>
2025-04-01 17:36:31 +0200

    feat: update getting a new month data

--- client/src/hooks/queries/useTimelineData.ts
+++ client/src/hooks/queries/useTimelineData.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getCurrentMonthData, getTimelineData } from "~/api/timeline";
+import { getToday } from "~/utils/helpers";
 
 // Get year data
 export function useTimelineData(userId: string, year: number) {
@@ -15,8 +16,10 @@ export function useTimelineData(userId: string, year: number) {
 
 // Get year data
 export function useCurrentMonthData(userId: string) {
+  const { currentMonth, currentYear } = getToday();
+
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["current-month"],
+    queryKey: ["current-month", currentMonth, currentYear],
     queryFn: () => getCurrentMonthData(userId),
     enabled: Boolean(userId),
   });
--- server/src/controllers/getCurrentMonthData.ts
+++ server/src/controllers/getCurrentMonthData.ts
@@ -1,17 +1,14 @@
 import { Request, Response } from "express";
 
 import { ITask, Task } from "../models/Task.js";
-import { IEntry } from "../models/Entry.js";
+import { Entry } from "../models/Entry.js";
 
-import {
-  filterTasksByMonth,
-  filterTasksByYear,
-  getEntriesByMonth,
-} from "../utils/handlers.js";
+import { filterTasksByMonth, filterTasksByYear } from "../utils/handlers.js";
 
-import { getToday } from "../utils/dates.js";
+import { getDaysInMonth, getToday } from "../utils/dates.js";
 import { mapEntry, mapTask } from "../utils/mappers.js";
 import { MonthTimelineData } from "../utils/types.js";
+import { NewEntryData } from "./createTask.js";
 
 // Get Timeline
 export async function getCurrentMonthData(req: Request, res: Response) {
@@ -24,6 +21,7 @@ export async function getCurrentMonthData(req: Request, res: Response) {
   }
 
   const { currentMonth, currentYear } = getToday();
+  const daysInMonth = getDaysInMonth(currentMonth, currentYear);
 
   try {
     const tasks: ITask[] = await Task.find({ userId })
@@ -34,23 +32,45 @@ export async function getCurrentMonthData(req: Request, res: Response) {
     const yearTasks = filterTasksByYear(tasks, currentYear);
     const monthTasks = filterTasksByMonth(yearTasks, currentMonth);
 
-    const data = monthTasks.map((task) => {
-      const entries = task.entries as IEntry[];
+    const data = await Promise.all(
+      monthTasks.map(async (task) => {
+        let taskEntries = await Entry.find({
+          userId,
+          taskId: task._id,
+          month: currentMonth,
+          year: currentYear,
+        })
+          .lean()
+          .exec();
 
-      const monthEntries = getEntriesByMonth(
-        entries,
-        currentMonth,
-        currentYear,
-      );
+        if (!taskEntries.length) {
+          const newMonthEntries = [];
 
-      const mappedTask = mapTask(task);
-      const mappedEntries = monthEntries.map(mapEntry);
+          for (let i = 1; i <= daysInMonth; i++) {
+            const entryData: NewEntryData = {
+              userId: userId as string,
+              taskId: task._id,
+              year: currentYear,
+              month: currentMonth,
+              day: i,
+              status: 0,
+            };
 
-      return {
-        task: mappedTask,
-        entries: mappedEntries,
-      };
-    });
+            const entry = new Entry(entryData);
+            await entry.save();
+
+            newMonthEntries.push(entry);
+          }
+
+          taskEntries = newMonthEntries;
+        }
+
+        return {
+          task: mapTask(task),
+          entries: taskEntries.map(mapEntry),
+        };
+      }),
+    );
 
     const currentMonthData: MonthTimelineData = {
       month: currentMonth,
@@ -68,3 +88,68 @@ export async function getCurrentMonthData(req: Request, res: Response) {
     }
   }
 }
+
+// export async function getCurrentMonthData(req: Request, res: Response) {
+//   const { userId } = req.query;
+
+//   if (!userId) {
+//     return res.status(500).json({
+//       message: "Some parameters are missing: userId.",
+//     });
+//   }
+
+//   const { currentMonth, currentYear } = getToday();
+
+//   try {
+//     const tasks: ITask[] = await Task.find({ userId })
+//       .populate("entries")
+//       .lean() // Converts to plain object
+//       .exec();
+
+//     const yearTasks = filterTasksByYear(tasks, currentYear);
+//     const monthTasks = filterTasksByMonth(yearTasks, currentMonth);
+
+//     const entries: IEntry[] = [];
+
+//     monthTasks.forEach(async (task) => {
+//       const taskEntries = await Entry.find({ userId, taskId: task._id })
+//         .lean()
+//         .exec();
+
+//       entries.push(taskEntries);
+//     });
+
+//     const data = monthTasks.map((task) => {
+//       const entries = task.entries as IEntry[];
+
+//       const monthEntries = getEntriesByMonth(
+//         entries,
+//         currentMonth,
+//         currentYear,
+//       );
+
+//       const mappedTask = mapTask(task);
+//       const mappedEntries = monthEntries.map(mapEntry);
+
+//       return {
+//         task: mappedTask,
+//         entries: mappedEntries,
+//       };
+//     });
+
+//     const currentMonthData: MonthTimelineData = {
+//       month: currentMonth,
+//       tasks: data,
+//     };
+
+//     return res.json(currentMonthData);
+//   } catch (err) {
+//     if (err instanceof Error) {
+//       console.log("🔴 Error:", err.message);
+
+//       return res.status(500).json({
+//         message: "Failed to fetch user's year data.",
+//       });
+//     }
+//   }
+// }
